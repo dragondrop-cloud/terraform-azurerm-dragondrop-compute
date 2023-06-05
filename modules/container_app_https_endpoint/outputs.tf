bffb01695595a364cf1e0ebd2b0a548e66f9ca9f
GoodmanBen <goodmanben@gmail.com>
2023-06-05 13:21:25 -0400

    build: outputs include container app query

--- modules/container_app_https_endpoint/outputs.tf
+++ modules/container_app_https_endpoint/outputs.tf
@@ -1,3 +1,7 @@
 output "https_url" {
   value = azurerm_container_app.dragondrop_https_trigger.ingress[0].fqdn
 }
+
+output "log_analytics_container_app_query" {
+  value = 'ContainerAppConsoleLogs_CL | where EnvironmentName_s == "${azurerm_container_app_environment.container_environment.name}" | project _timestamp_d, ContainerName_s, EnvironmentName_s, Log_s'
+}
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,11 @@
+output "container_instance_id" {
+  value = module.container_instance_dragondrop_engine.container_instance_id
+}
+
 output "https_url" {
   value = module.container_app_https_endpoint.https_url
 }
 
-output "container_instance_id" {
-  value = module.container_instance_dragondrop_engine.container_instance_id
+output "log_analytics_container_app_query" {
+  value = module.container_app_https_endpoint.log_analytics_container_app_query
 }
